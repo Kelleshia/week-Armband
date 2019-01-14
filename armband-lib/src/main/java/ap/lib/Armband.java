6c8479614a6e100accc2df6ae9798201c22043fd
Kelleshia <kelleshiakinlocke@gmail.com>
2019-01-14 16:33:05 -0500

    Armband Machines

--- armband-lib/src/main/java/ap/lib/Armband.java
+++ armband-lib/src/main/java/ap/lib/Armband.java
@@ -2,4 +2,41 @@ package ap.lib;
 
 public class Armband {
 
+	private int id;
+	private String color;
+	private float price;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Armband [id=" + id + ", color=" + color + ", price=" + price
+				+ "]";
+	}
+	public Armband(int id, String color, float price) {
+		super();
+		this.id = id;
+		this.color = color;
+		this.price = price;
+	}
+	
+	
 }
---
+++ armband-lib/src/main/java/ap/lib/IArmbandMachines.java
@@ -0,0 +1,11 @@
+package ap.lib;
+
+public interface IArmbandMachines {
+/**
+ * Calculates the cost of an armband
+ * @param price -cost of the armband
+ * @param quantity - amount of armbands
+ */
+	
+	float getCost(float price, int quantity);
+}
